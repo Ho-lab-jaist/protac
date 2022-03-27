8311cb4ede84299242ec9c4f46cb864b95dfbf86
quan-luu <luuquan.bku@gmail.com>
2022-03-27 14:38:18 +0900

    upload protac description package

---
+++ protac_description/launch/protac_gazebo_velocity.launch.py
@@ -0,0 +1,87 @@
+import os.path
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    robot_name = "protac"
+    package_name = robot_name + "_description"
+    rviz_config = os.path.join(get_package_share_directory(
+        package_name), "launch", robot_name + ".rviz")
+    robot_description = os.path.join(get_package_share_directory(
+        package_name), "urdf", robot_name + "_gazebo_velocity.urdf.xacro")
+    robot_description_config = xacro.process_file(robot_description)
+
+    # robot_description_config = xacro.parse(open(robot_description))
+    # xacro.process_doc(robot_description_config)
+
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-topic", "robot_description",
+                   "-entity", "protac"],
+        output="screen"
+    )
+
+    # Launching gazebo.launch.py is comsumed more than 1 minute somehow...
+    joint_state_broadcaster = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "start", "--spin-time", "120",
+             "joint_state_broadcaster"],
+        output="screen"
+    )
+
+    velocity_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start', 'velocity_controller'],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster],
+            )
+        ),
+
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster,
+                on_exit=[velocity_controller],
+            )
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory("gazebo_ros"), "launch"), "/gazebo.launch.py"]),
+        ),
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[
+                {"robot_description": robot_description_config.toxml()}],
+            output="screen"),
+
+        spawn_entity,
+
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=["-d", rviz_config],
+            output={
+                "stdout": "screen",
+                "stderr": "log",
+            },
+        )
+    ])
